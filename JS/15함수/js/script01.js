4fffbb0b41142f8ca1f7bc52723a89ea42c67e8f
sdee96 <155033213+sdee96@users.noreply.github.com>
2024-02-19 13:44:08 +0900

    Add files via upload

---
+++ JS/15함수/js/script01.js
@@ -0,0 +1,8 @@
+function f1(){
+    console.log('script01_f1 Function call!');
+
+
+}
+const f1_01=()=>{
+console.log('script01_f1_01 function call');
+}
---
+++ JS/15함수/js/script02.js
@@ -0,0 +1,7 @@
+function f1(){
+    console.log('script02_f1 Function call!');
+}
+
+const f1_01=()=>{
+    console.log('script02_f1_01 fucntion call')
+}
